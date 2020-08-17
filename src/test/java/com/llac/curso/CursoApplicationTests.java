5b4a941b444b8c36ddea6581b9900b3939385ddc
llac78 <llac78@gmail.com>
2020-08-17 00:16:04 -0300

    Criação do projeto

---
+++ src/test/java/com/llac/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llac.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
