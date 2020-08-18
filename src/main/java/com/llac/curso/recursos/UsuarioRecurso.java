2c193182b011b21c6fd8a63e5b8ba3691b5c6e61
llac78 <llac78@gmail.com>
2020-08-18 00:49:04 -0300

    Implementação da camada Service e registro de componente

--- src/main/java/com/llac/curso/recursos/UsuarioRecurso.java
+++ src/main/java/com/llac/curso/recursos/UsuarioRecurso.java
@@ -1,20 +1,35 @@
 package com.llac.curso.recursos;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.llac.curso.entidades.Usuario;
+import com.llac.curso.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuarios")
-public class UsuarioRecurso {
+public class UsuarioRecurso { //Controller
+	
+	@Autowired
+	private UsuarioService service;
 
 	@GetMapping
-	public ResponseEntity<Usuario> listar(){
-		Usuario u = new Usuario(1L, "MAria", "maria@gmail.com", "999999999", "12345");
+	public ResponseEntity<List<Usuario>> listar(){
+		List<Usuario> lista = service.listar();
 		
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(lista);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id){ // @PathVariable para aceitar o {id} como parâmetro
+		Usuario usuario = service.buscarPorId(id);
+		
+		return ResponseEntity.ok().body(usuario);
 	}
 }
---
+++ src/main/java/com/llac/curso/services/UsuarioService.java
@@ -0,0 +1,27 @@
+package com.llac.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.llac.curso.entidades.Usuario;
+import com.llac.curso.repositorios.UsuarioRepositorio;
+
+@Service
+public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepositorio repositorio;
+	
+	public List<Usuario> listar(){
+		return repositorio.findAll();
+	}
+
+	public Usuario buscarPorId(Long id) {
+		Optional<Usuario> usuario = repositorio.findById(id);
+		
+		return usuario.get(); // o método get() retorna o objeto do tipo contido no Optional, no caso, tipo Usuario 
+	}
+}
